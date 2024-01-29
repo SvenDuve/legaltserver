d2dd67bd95c7d30c585889bb1de1f19bb6b3fffe
Sven Duve <svenduve@gmail.com>
2024-01-29 11:04:54 +0100

    added further client reports

--- index.js
+++ index.js
@@ -511,6 +511,280 @@ app.post('/api/clients/data/A', async (req, res) => {
   
 
 
+app.post('/api/clients/data/B', async (req, res) => {
+    const { startDate, endDate, client }  = req.body;
+
+
+    let result = await db.query(`SELECT DISTINCT department FROM time_entries WHERE client = $1`, [client]);
+    const departments = result.rows.map(row => row.department);
+
+
+    const sqlProjects = `
+        SELECT DISTINCT
+        project
+        FROM time_entries 
+        WHERE client = $1
+        AND department = $2
+        AND start_time >= $3
+        AND end_time <= $4
+        ORDER BY project`;
+
+    let departmentProjects = {};
+    let errorOccurred = null;
+
+    for (const department of departments) {
+        try {
+            const resultProjects = await db.query(sqlProjects, [client, department, startDate, endDate]);
+            const projects = resultProjects.rows.map(row => row.project);
+            departmentProjects[department] = projects;
+
+            // ...
+        } catch (err) {
+            errorOccurred = err
+            break;
+        }
+    }
+
+    const sql = `
+        SELECT 
+        id, 
+        pid, 
+        client, 
+        department, 
+        project, 
+        counterparty, 
+        description, 
+        start_time, 
+        end_time,
+        TO_CHAR(end_time - start_time, 'HH24:MI') AS time_diff_hrs_mins,
+        ROUND(EXTRACT(EPOCH FROM (end_time - start_time)) / 3600.0, 2) AS time_diff_decimal
+        FROM time_entries 
+        WHERE client = $1
+        AND department = $2
+        AND project = $3
+        AND start_time >= $4
+        AND end_time <= $5
+        ORDER BY counterparty, start_time`;
+
+    const sqlProjectSeconds = `
+        SELECT
+        SUM(EXTRACT(EPOCH FROM (end_time - start_time))) AS total_time_diff_seconds
+        FROM time_entries 
+        WHERE client = $1 
+        AND department = $2
+        AND project = $3
+        AND start_time >= $4 
+        AND end_time <= $5`;
+
+    const sqlDeptSeconds = `
+        SELECT
+        SUM(EXTRACT(EPOCH FROM (end_time - start_time))) AS total_time_diff_seconds
+        FROM time_entries 
+        WHERE client = $1 
+        AND department = $2
+        AND start_time >= $3 
+        AND end_time <= $4`;
+
+    const totalSumSeconds = `
+        SELECT
+        SUM(EXTRACT(EPOCH FROM (end_time - start_time))) AS total_time_diff_seconds
+        FROM time_entries 
+        WHERE client = $1 
+        AND start_time >= $2 
+        AND end_time <= $3`;
+
+
+    // Create an empty array to store the entries
+    let allEntries = [];
+    let allProjectEntriesHrsMins = [];
+    let allProjectDecimalEntriesHrsMins = [];
+    let allDeptEntriesHrsMins = [];
+    let allDeptDecimalEntriesHrsMins = [];
+    errorOccurred = null;
+
+
+    // ...
+
+    let deptProjectEntries = {};
+
+    for (const department of departments) {
+        let projects = departmentProjects[department];
+        if (!deptProjectEntries[department]) {
+            deptProjectEntries[department] = {};
+        }
+        for (const project of projects) {
+
+            try {
+                const result = await db.query(sql, [client, department, project, startDate, endDate]);
+                const entries = result.rows.map(row => ({
+                    ...row,
+                    client: clientsMap[row.client] // Convert client label to value using clientsMap
+                }));
+                allEntries = allEntries.concat([entries]);
+
+                if (!deptProjectEntries[department][project]) {
+                    deptProjectEntries[department][project] = [];
+                }
+                
+                deptProjectEntries[department][project] = deptProjectEntries[department][project].concat([entries]);
+
+
+                const resultSumSeconds = await db.query(sqlProjectSeconds, [client, department, project, startDate, endDate]);
+                const totalSeconds = resultSumSeconds.rows[0].total_time_diff_seconds;
+        
+                const hours = Math.floor(totalSeconds / 3600);
+                const minutes = Math.floor((totalSeconds % 3600) / 60);
+                const totalHrsMins = `${hours}:${minutes.toString().padStart(2, '0')}`;
+
+                const totalDecimalHours = (totalSeconds / 3600).toFixed(2);
+
+                allProjectEntriesHrsMins = allProjectEntriesHrsMins.concat([totalHrsMins]);
+                allProjectDecimalEntriesHrsMins = allProjectDecimalEntriesHrsMins.concat([totalDecimalHours]);
+
+                // ...
+            } catch (err) {
+                errorOccurred = err
+                break;
+            }
+        }
+
+        try {
+            // Sum up all projects for the department
+            const resultSumSeconds = await db.query(sqlDeptSeconds, [client, department, startDate, endDate]);
+            const totalSeconds = resultSumSeconds.rows[0].total_time_diff_seconds;
+    
+            const hours = Math.floor(totalSeconds / 3600);
+            const minutes = Math.floor((totalSeconds % 3600) / 60);
+            const totalHrsMins = `${hours}:${minutes.toString().padStart(2, '0')}`;
+
+            const totalDecimalHours = (totalSeconds / 3600).toFixed(2);
+
+            allDeptEntriesHrsMins = allDeptEntriesHrsMins.concat([totalHrsMins]);
+            allDeptDecimalEntriesHrsMins = allDeptDecimalEntriesHrsMins.concat([totalDecimalHours]);
+
+            // ...
+        } catch (err) {
+            errorOccurred = err
+            break;
+        }
+
+
+
+    }
+
+    const resultSumSeconds = await db.query(totalSumSeconds, [client, startDate, endDate]);
+    const totalSeconds = resultSumSeconds.rows[0].total_time_diff_seconds;
+
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const totalHrsMins = `${hours}:${minutes.toString().padStart(2, '0')}`;
+
+    const totalDecimalHours = (totalSeconds / 3600).toFixed(2);
+
+    if (errorOccurred) {
+        res.status(500).json({ error: errorOccurred.message });
+    } else {
+        res.json({
+            success: true,
+            entries: allEntries,
+            deptProjectEntries: deptProjectEntries,
+            projHrsMins: allProjectEntriesHrsMins,
+            projDecHrsMins: allProjectDecimalEntriesHrsMins,
+            deptHrsMins: allDeptEntriesHrsMins,
+            deptDecHrsMins: allDeptDecimalEntriesHrsMins,
+            totalHrsMins: totalHrsMins,
+            totalDecHrsMins: totalDecimalHours
+        });
+
+    }
+
+});
+
+
+app.post('/api/clients/data/AnnexTable', async (req, res) => {
+    const { startDate, endDate, client }  = req.body;
+
+
+    const sql = `
+    SELECT DISTINCT project
+    FROM time_entries
+    WHERE client = $1
+    AND start_time >= $2
+    AND end_time <= $3
+    ORDER BY project;
+    `;
+
+    let result = await db.query(sql, [client, startDate, endDate]);
+
+    const sqlProjectTimes = `
+    SELECT
+    project,
+    counterparty,
+    SUM(EXTRACT(EPOCH FROM (end_time - start_time))) AS total_time_diff
+    FROM time_entries
+    WHERE client = $1
+    AND project = $2
+    AND start_time >= $3
+    AND end_time <= $4
+    GROUP BY project, counterparty
+    ORDER BY project, counterparty;
+    `;
+
+    let projectTimes = {};
+    let errorOccurred = null;
+    
+    for (const project of result.rows) {
+        try {
+            const resultTimes = await db.query(sqlProjectTimes, [client, project.project, startDate, endDate]);
+            const times = resultTimes.rows.map(row => row);
+            console.log(times)
+            
+            projectTimes[project.project] = times.map(row => {
+                    const totalSeconds = row.total_time_diff;
+                    const hours = Math.floor(totalSeconds / 3600);
+                    const minutes = Math.floor((totalSeconds % 3600) / 60);
+                    const totalHrsMins = `${hours}:${minutes.toString().padStart(2, '0')}`;
+                    return { ...row, total_time_diff: totalHrsMins };
+            });
+
+            // const PORT = process.env.PORT || 3000;
+            // app.listen(PORT, () => {
+            //     console.log(`Server is running on port ${PORT}`);
+            // });
+            // console.log(times)
+            // projectTimes[project.project] = times;
+
+            // ...
+        } catch (err) {
+            errorOccurred = err
+            break;
+        }
+    }   
+
+    console.log(projectTimes);
+
+    if (errorOccurred) {
+        res.status(500).json({ error: errorOccurred.message });
+    } else {
+        res.json({
+            success: true,
+            projectTimes: projectTimes,
+            // deptProjectEntries: deptProjectEntries,
+            // projHrsMins: allProjectEntriesHrsMins,
+            // projDecHrsMins: allProjectDecimalEntriesHrsMins,
+            // deptHrsMins: allDeptEntriesHrsMins,
+            // deptDecHrsMins: allDeptDecimalEntriesHrsMins,
+            // totalHrsMins: totalHrsMins,
+            // totalDecHrsMins: totalDecimalHours
+        });
+
+    }
+
+
+});
+  
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
